0ec044bff921889de83278f6553dffdc4d78f107
Edwin Saul <edwinsaulpm@gmail.com>
2023-07-10 16:44:01 -0500

    nuevo ejemplo flutter

---
+++ app_flutter/lib/main.dart
@@ -0,0 +1,25 @@
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(HelloWorldApp());
+}
+
+class HelloWorldApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Hola mundo desde dart!'),
+        ),
+        body: Center(
+          child: Text(
+            'Hello, World!',
+            style: TextStyle(fontSize: 24),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ app_flutter/lib/main_staging.dart
@@ -0,0 +1,6 @@
+import 'package:app_flutter/app/app.dart';
+import 'package:app_flutter/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
